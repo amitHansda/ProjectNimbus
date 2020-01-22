f4b3c2974f6897588a17116bc9d9249739bfe452
amith <amith@LAPTOP-MG9I2R9V>
2020-01-23 00:57:22 +0530

    initial commit

---
+++ src/Game/Game.ActorModel/Messages/JoinGameMessages.cs
@@ -0,0 +1,11 @@
+﻿namespace Game.ActorModel.Messages
+{
+    public class JoinGameMessages
+    {
+        public string PlayerName { get; private set; }
+        public JoinGameMessages(string playerName)
+        {
+            PlayerName = playerName;
+        }
+    }
+}
---
+++ src/Game/Game.ActorModel/Messages/PlayerStatusMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Game.ActorModel.Messages
+{
+    public class PlayerStatusMessage
+    {
+        public PlayerStatusMessage(string playerName,int health)
+        {
+            PlayerName = playerName;
+            Health = health;
+        }
+
+        public string PlayerName { get; private set; }
+        public int Health { get; private set; }
+    }
+}
